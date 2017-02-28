5f2bbcc4a2b3cece9ea25a9df414b9fcde1d033e
yixinzhang <yzha664@uwo.ca>
2017-02-28 11:50:04 -0500

    CS2212: Login_page

---
+++ Michael/login_page/login.js
@@ -0,0 +1,60 @@
+/*
+ * Created by Michael Yixin Zhang
+ */
+
+ 
+function login() {
+    var a,b,x;
+    var email = document.getElementById("email");
+    var pass = document.getElementById("password");
+    //var txt = '{ "user" : [' +
+    //'{ "email":"austin" , "password":"123456" },' +
+    //'{ "email":"michael" , "password":"123456" },' +
+    //'{ "email":"casey" , "password":"123456" },' +
+    //'{ "email":"will" , "password":"123456" },' +
+    //'{ "email":"caocao" , "password":"123456" },' +
+    //'{ "email":"christine" , "password":"123456" },' +
+    //'{ "email":"vivian" , "password":"123456" },' +
+    //'{ "email":"maya" , "password":"123456" } ]}';
+    
+    var txt = {
+        "user":[
+                { "email":"austin", "password":"123456" },
+                { "email":"maya", "password":"123456" },
+                { "email":"casey", "password":"123456" },
+                { "email":"will", "password":"123456" },
+                { "email":"caocao", "password":"123456" },
+                { "email":"christine", "password":"123456" },
+                { "email":"vivian", "password":"123456" },
+                { "email":"michael", "password":"123456" }
+        ]
+    }
+    
+    var obj = eval(txt);
+    //var obj = JSON.parse(txt);
+    for( x in obj.user){
+        a = obj.user[x].email;
+        b = obj.user[x].password;
+        
+    }
+    
+
+    
+    if (email.value == "") {
+  
+        alert("Please enter your uwo email");
+  
+    } else if (pass.value  == "") {  
+  
+        alert("Please enter your password");
+  
+    } else if(email.value ==  a && pass.value == b ){
+  
+        window.location.href="welcome.html";  
+  
+    } else {  
+  
+        alert("Please enter a valid uwo email or password.")
+  
+    }  
+}  
