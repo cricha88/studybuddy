083d7f20cc4d8cdeab065d8c594126f727f9920a
Austin <austinabell8@gmail.com>
2017-03-03 21:37:28 -0500

    Added a bunch of changes

--- StudyBuddy/grails-app/init/studybuddy/BootStrap.groovy
+++ StudyBuddy/grails-app/init/studybuddy/BootStrap.groovy
@@ -3,9 +3,12 @@ package studybuddy
 class BootStrap {
 
     def init = { servletContext ->
-        def userCourseComponent = new UsersCourseComponents(username:"Austin" , courseComponentId: "not null")
-        userCourseComponent.save()
+
     }
     def destroy = {
+    }
+
+    def hackSave(){
+        this.save()
     }
 }
