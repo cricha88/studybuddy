4a542839a85b9a3fcbf7e27b1e3601003631426f
Austin <austinabell8@gmail.com>
2017-03-25 00:00:09 -0400

    final

--- StudyBuddy/grails-app/domain/studybuddy/CourseComponentDateTimes.groovy
+++ StudyBuddy/grails-app/domain/studybuddy/CourseComponentDateTimes.groovy
@@ -1,13 +1,34 @@
 package studybuddy
 
-class CourseComponentDateTimes {
+import org.apache.commons.lang.builder.HashCodeBuilder
+
+class CourseComponentDateTimes implements Serializable {
     String courseComponentId
     String dayOfWeek
     String startTime
     String endTime
 
+
+    boolean equals(other) {
+        if (!(other instanceof CourseComponentDateTimes)) {
+            return false
+        }
+
+        other.courseComponentId == courseComponentId && other.dayOfWeek == dayOfWeek
+    }
+
+    int hashCode() {
+        def builder = new HashCodeBuilder()
+        builder.append courseComponentId
+        builder.append dayOfWeek
+        builder.append startTime
+        builder.append endTime
+        builder.toHashCode()
+    }
+
     static mapping = {
-        id column: 'course_component_id', generator: 'assigned', name: 'courseComponentId'
+        id composite: ['courseComponentId', 'dayOfWeek'], generator: 'assigned'
         version false
     }
+
 }
